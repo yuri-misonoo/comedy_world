fa8c56a4136df60ab76d890931bf5c0ea66ac732
yuri-misonoo <yuri.panpanda@gmail.com>
2021-10-23 05:54:38 +0000

    [add]ヘッダー、トップページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+  get '/signin' => 'homes#signin'
+  get '/signup' => 'users#new'
 end
