fcaa4454255af4441c66f57381572c9348c81acf
yuri-misonoo <yuri.panpanda@gmail.com>
2021-10-25 11:53:55 +0000

    [add]postコントローラ、モデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_011418) do
+ActiveRecord::Schema.define(version: 2021_10_25_115144) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "bo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
